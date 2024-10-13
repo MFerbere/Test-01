1645a6e60a5646e657d1b47917fa792652f17c57
MFerbere <milla.smartt2@mail.dcu.ie>
2024-10-13 19:01:00 +0100

    Added simple code to test modification

--- Testcode.py
+++ Testcode.py
@@ -1,1 +1,18 @@
-print('Hello world')
+
+a = int(input())
+b = int(input())
+
+
+fun = str(input("+, -, /, x: "))
+
+if fun == '+':
+    print(a+b)
+
+if fun == '-':
+    print(a-b)
+
+if fun == '/':
+    print(a/b)
+
+if fun == 'x':
+    print(a*b)
